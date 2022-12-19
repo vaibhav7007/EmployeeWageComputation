e38a5cce7416eb0c3779d6ed6e3191093edd027a
vaibhav <vaibhavmehta524@gmail.com>
2022-12-20 02:12:23 +0530

    Adding EmployeeAttendence code-UC1

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_PRESENT=1;
+            Console.WriteLine("Welcome to EmployeeWageComputation");
+            //UC-1 Check employee present or not
+            Random random = new Random();
+            int employeeInput=random.Next(0,2);
+
+            if (IS_PRESENT == employeeInput)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadLine();
+        }
+    }
+}
