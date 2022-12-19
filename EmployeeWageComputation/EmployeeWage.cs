f8575135ffe228f9dabc6bfdfed1862f72e093df
vaibhav <vaibhavmehta524@gmail.com>
2022-12-20 02:36:38 +0530

    Modifying EmployeeWage.cs to calculating wage

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -9,17 +9,30 @@ namespace EmployeeWageComputation
     class Program
     {
         static void Main(string[] args)
-        {
-            const int IS_PRESENT=1;
+        { 
             Console.WriteLine("Welcome to EmployeeWageComputation");
+            const int IS_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0,empWage=0;
+
             //UC-1 Check employee present or not
             Random random = new Random();
             int employeeInput=random.Next(0,2);
 
             if (IS_PRESENT == employeeInput)
+            {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("Employee is absent");
+                //empHrs = 0;
+            }
+
+            //UC-2 calculate EmployeeWage
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage is:" + empWage);
             Console.ReadLine();
         }
     }
