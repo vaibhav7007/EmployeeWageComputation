9d8f6e4e1e8ae633f09b36d9a366aa99482f9304
vaibhav <vaibhavmehta524@gmail.com>
2022-12-20 01:38:59 +0530

    Adding Welcome Message

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWageComputation");
+            Console.ReadLine();
+        }
+    }
+}
