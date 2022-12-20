5c16c10a289eb3b5a1b8ceb1d2acc3a6b5d1bd34
vaibhav <vaibhavmehta524@gmail.com>
2022-12-20 10:20:43 +0530

    Modifying EmployeeWage.cs to EmpMonth wage

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -9,33 +9,37 @@ namespace EmployeeWagesComputation
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0, empwage = 0;
+            const int MAX_WORKING_DAYS = 20;
+            int empHrs = 0, empwage = 0, totalwage = 0, day = 1;
             Console.WriteLine("Welcome to the employee Wages Computation Practices");
             //UC1 Employee attendance
             Random random = new Random();
-            int empInput = random.Next(0, 3);
 
-            switch (empInput)
+            for (day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case 1:
-                    Console.WriteLine("Employee is Full time");
-                    empHrs = 8;
-                    break;
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is Full time");
+                        empHrs = 8;
+                        break;
 
-                case 2:
-                    Console.WriteLine("Employee is part time");
-                    empHrs = 4;
-                    break;
+                    case 2:
+                        Console.WriteLine("Employee is part time");
+                        empHrs = 4;
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is absent");
-                    empHrs = 0;
-                    break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHrs = 0;
+                        break;
+                }
+                //Uc2 Calculate daily wages
+                empwage = EMP_RATE_PER_HR * empHrs;
+                totalwage += empwage;
             }
-
-            //Uc2 Calculate daily wages
-            empwage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Daily wage is: " + empwage);
+            Console.WriteLine("Total wage for {0} days:{1}", (day - 1), totalwage);
             Console.ReadLine();
         }
     }
