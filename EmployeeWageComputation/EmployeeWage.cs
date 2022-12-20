ce66b583f1215f922ba08171202e2d79225b108f
vaibhav <vaibhavmehta524@gmail.com>
2022-12-20 10:01:49 +0530

    Modifying EmployeeWage.cs to PartTime wage

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -1,38 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EmployeeWageComputation
+namespace EmployeeWagesComputation
 {
-    class Program
+    internal class EmpWage
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Welcome to EmployeeWageComputation");
-            const int IS_PRESENT = 1;
+        {
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0,empWage=0;
-
-            //UC-1 Check employee present or not
+            int empHrs = 0, empwage = 0;
+            Console.WriteLine("Welcome to the employee Wages Computation Practices");
+            //UC1 Employee attendance
             Random random = new Random();
-            int employeeInput=random.Next(0,2);
+            int empInput = random.Next(0, 3);
 
-            if (IS_PRESENT == employeeInput)
+            if (FULL_TIME == empInput)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is Full time");
                 empHrs = 8;
+            }
+            else if (PART_TIME == empInput)
+            {
+                Console.WriteLine("Employee is part time");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
-                //empHrs = 0;
+                empHrs = 0;
             }
 
-            //UC-2 calculate EmployeeWage
-            empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Employee Wage is:" + empWage);
+
+            //Uc2 Calculate daily wages
+            empwage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily wage is: " + empwage);
             Console.ReadLine();
         }
     }
